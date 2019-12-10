2cadbad12c230ee05190cb7f669ade082f6da8a1
Siam Mridha <masudalamsiam@gmail.com>
2019-12-10 17:30:28 -0500

    no message

--- src/index.js
+++ src/index.js
@@ -6,28 +6,23 @@ function App() {
     const localVideo = useRef(null)
     const remoteVideo = useRef(null)
     const textRef = useRef(null)
-    const pc = new RTCPeerConnection(null)
-
-    useEffect(() => {
-        // triggered when a new candidate is returned
-        pc.onicecandidate = (e) => {
-            // send the candidates to the remote peer
-            // see addCandidate below to be triggered on the remote peer
-            if (e.candidate) {
-                console.log(JSON.stringify(e.candidate))
-            }
+    const pc = new RTCPeerConnection()
+    pc.addEventListener('icecandidate', (e) => {
+        if (e.candidate) {
+            console.log('ICE Candidate:', JSON.stringify(e.candidate))
         }
-
-        // triggered when there is a change in connection state
-        pc.oniceconnectionstatechange = (e) => { console.log(e) }
-
-        // triggered when a stream is added to pc, see below - pc.addStream(stream)
-        pc.onaddstream = (e) => remoteVideo.current.srcObject = e.stream
-
+    })
+    pc.addEventListener('iceconnectionstatechange', (e) => { console.log('IC Connection State Change:', e) })
+    pc.addEventListener('icecandidateerror', (e) => { console.log('IC Candidate Error:', e) })
+    pc.addEventListener('track', ({ streams: [stream] }) => { remoteVideo.current.srcObject = stream })
+    useEffect(() => {
         navigator.mediaDevices.getUserMedia({ audio: true, video: true })
             .then((stream) => {
                 localVideo.current.srcObject = stream
-                pc.addStream(stream)
+                // pc.addStream(stream)
+                for (const track of stream.getTracks()) {
+                    pc.addTrack(track, stream)
+                }
             }).catch((error) => {
                 alert(`getUserMedia() error: ${error}`)
             })
@@ -62,12 +57,9 @@ function App() {
     const setRemoteDescription = () => {
         // retrieve and parse the SDP copied from the remote peer
         const desc = JSON.parse(textRef.current.value)
-        console.log('Adding candidate:', desc)
         // set sdp as remote description
         pc.setRemoteDescription(new RTCSessionDescription(desc))
-            .then(() => {
-
-            }).catch((error) => {
+            .catch((error) => {
                 console.log('setRemoteDescription', error)
             })
     }
@@ -75,30 +67,22 @@ function App() {
     const addCandidate = () => {
         // retrieve and parse the Candidate copied from the remote peer
         const candidate = JSON.parse(textRef.current.value)
-        console.log('Adding candidate:', candidate)
         // add the candidate to the peer connection
         pc.addIceCandidate(new RTCIceCandidate(candidate))
-            .then(() => {
-
-            }).catch((error) => {
+            .catch((error) => {
                 console.log('addCandidate', error)
             })
     }
 
     return (
         <div className="App">
-            <video className="video" ref={localVideo} autoPlay muted />
-            <video className="video" ref={remoteVideo} autoPlay />
-            <br />
-
+            <video className="localVideo" ref={localVideo} autoPlay muted />
+            <video className="remoteVideo" ref={remoteVideo} autoPlay />
             <button onClick={createOffer}>Offer</button>
             <button onClick={createAnswer}>Answer</button>
-
-            <br />
-            <textarea style={{ width: 450, height: 40 }} ref={textRef} />
-            <br />
             <button onClick={setRemoteDescription}>Set Remote Desc</button>
             <button onClick={addCandidate}>Add Candidate</button>
+            <textarea ref={textRef} />
         </div>
     );
 }
